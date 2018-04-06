d8fe64f37034a71401dbe360b195a47608d32a67
abelko <esung1129@gmail.com>
2018-04-06 18:28:03 +0900

    저녁 기능 추가

--- bot/index.js
+++ bot/index.js
@@ -8,15 +8,13 @@ const { getMenuDaily, testGetMenuDailyDB, addStock, substractStock } = require('
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
+  const jinsuSays = '';
 
-  menuDailyLunch = await getMenuDaily('LUNCH', 'seoul-2', '2018-01-24');
-  menuDailyDinner = await getMenuDaily('DINNER', 'seoul-2', '2018-01-24');
-
-  function checkZeroStock(menuDailyServiceType) {
-    return menuDailyServiceType.filter(menu => menu.remain === 0);
-  }
-
+  const menuDailyLunch = await getMenuDaily('LUNCH', 'seoul-2', '2018-01-24');
+  const menuDailyDinner = await getMenuDaily('DINNER', 'seoul-2', '2018-01-24');
+  
   const filteredMenuDailyLunch = checkZeroStock(menuDailyLunch);
+  const filteredMenuDailyDinner = checkZeroStock(menuDailyDinner);
 
   // 런치 재고 확인
   filteredMenuDailyLunch.map((menuLunch) => {
@@ -48,60 +46,67 @@ bot.on('message', async (msg) => {
             const controlNumber = 1;
             console.log(` => 저녁(id : ${menuDinnerId})의 수량이 1개여서, 점심 1 올리고 저녁은 0으로 하겠습니당`);
           
-            //점심 올리는 함수 
             addStock(menuLunchId, initialLunchStock, controlNumber)
-            //저녁 내리는 함수
             substractStock(menuDinnerId, initialDinnerStock, controlNumber)
-            //console.log('==================================================================');
           }
-          else {
+          if (menuDinner.remain > 1) {
             const controlNumber = menuDinner.remain - 1; //4개를 옮겨주어야 한다.
             console.log(` => 점심의 ${menuLunchName}(${menuLunchId})는 ${controlNumber}만큼 올리고, 저녁 ${menuDinnerName}(${menuDinnerId})는 ${controlNumber}만큼 내립니다.`);
 
-            // 점심 올리는 함수 //
             addStock(menuLunchId, initialLunchStock, controlNumber)
-            // 저녁 내리는 함수 
             substractStock(menuDinnerId, initialDinnerStock, controlNumber)
-            //console.log('==================================================================');
+            
           }
-
         }
-        //  else if (menuLunchName) {
-        //   // 0 0 으로 설정해 놓아서, api에 아예 메뉴가 안들어옴 ( 앱에도 안보임 ) // 그냥 아무 처리도 하지 말까...?
-        //   console.log('런치는 매진인데, 디너에는 메뉴가 없습니다.')
-        //   console.log('==================================================================');
-        // }
       })
-
-
       //예외 처리. 점심과 저녁의 메뉴가 안겹칠 수 도있다. 
-      //예외 처리. 저녁도 0이면 아무것도 안한다.
       //저녁데일리메뉴의 menuLunch(현재 돌고 있는) 수량을 1개만 남기고,  - 비동기 처리 해야함
-      //dinner 수량이 1개 밖에 안남아 있다면, 디너는 0을 만들고 lunch 수량은 1만 올린다.
   });
   
-  console.log('=========================== FINISH ===============================');
+  console.log('===================== LUNCH CHECK FINISH =========================');
 
 
+  filteredMenuDailyDinner.map((menuDinner) => {
+    const menuDinnerName = menuDinner.menuInformation.name.short;
+    const menuLunch = findSameMenu(menuDinnerName, menuDailyLunch);
+    const menuLunchName = menuLunch.menuInformation.name.short;
+    const menuDinnerId = menuDinner.idx;
+    const menuLunchId = menuLunch.idx;
+    const initialDinnerStock = menuDinner.stock;
+    const initialLunchStock = menuLunch.stock;
 
-  // 디너 재고확인
-  // menuDailyDinner.map((menuDinner) => {
-  //   if(menuDailyDinner.remain === 0) {
-  //     //저녁데일리메뉴의
-  //   }
-  // });
+    if(menuDinner.stock === 99) return; // 99이면 운영팀에서 일부로 없앤거기 때문에 return한다.
+    console.log('==================================================================');
+    console.log(`저녁 ${menuDinnerName} 메뉴가 매진 입니다.(메뉴번호 : ${menuDinner.menuIdx})`);
+    if(menuLunch.remain === 0 || menuLunch.remain === 1) {
+      console.log(` => 점심 ${menuDinnerName} 메뉴도 ${menuLunch.remain}개 뿐이여서 저녁으로 옮길 수 없습니다 ㅠㅠ`);
+    } 
+    
+    // 런치의 재고가 1개 초과일 때
+    if(menuLunch.remain > 1) {
+      console.log(`저녁 ${menuDinnerName}의 재고(${menuDinnerId})가 0 임으로, 점심 ${menuLunchName}(${menuLunchId}) 수량을 저녁으로 옮겨야 합니다. `);
+      const controlNumber = 1;
 
-  //testGetMenuDailyDB();
+      addStock(menuDinnerId, initialDinnerStock, controlNumber);
+      substractStock(menuLunchId, initialLunchStock, controlNumber);
+      //sendmessage funciton 을 만들자.
+      console.log(`송파 Dinner에 ${menuDinnerName}가 매진이 되어 LUNCH에서 ${controlNumber}개 차감하고 DINNER에 ${controlNumber}개 추가 했습니다 \r`)
+      // console.log(jinsuSays)
+    }
+  })
 
+  function findSameMenu(menuName, menuDailyRawData) {
+    return menuDailyRawData.filter(menu => menu.menuInformation.name.short === menuName)[0];
+  } 
 
+  function checkZeroStock(menuDailyServiceType) {
+    return menuDailyServiceType.filter(menu => menu.remain === 0);
+  }
 
-
-
-
-
-  console.log(process.env.MYSQL_USER);
   bot.sendMessage(chatId, 'Received your message');
 });
+
+//"역삼 Lunch에 치킨벤또가 매진이 되어 Dinner에서 2개를 차감하고 Lunch에 2개를 추가했습니다")
 
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
